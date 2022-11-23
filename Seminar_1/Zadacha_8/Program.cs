91dd87b50b99c288116a77dac5c823b10ea1ff69
Taissera <taissera@ya.ru>
2022-11-23 18:05:22 +0300

    HW for seminar 1

---
+++ Seminar_1/Zadacha_8/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+Console.Write($"чётные числа от 1 до {number}: ");
+while (i < number)
+{
+    i += 1;
+    if (i %2 == 0)
+    {
+    Console.Write($"{i} ");
+    }
+}
+
+

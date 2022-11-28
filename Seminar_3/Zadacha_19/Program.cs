ce8cd74cd78bc9e153530b03c4c5b6775625118a
Taissera <taissera@ya.ru>
2022-11-28 23:17:23 +0300

    HW_Seminar_3

--- Seminar_3/Zadacha_19/Program.cs
+++ Seminar_3/Zadacha_19/Program.cs
@@ -14,14 +14,14 @@ if (NumLength == 5)
 {
     if (number[0] == number[4] && number[1] == number[3])
     {
-        Console.WriteLine($"{number} - палиндром");
+        Console.WriteLine($"Да. {number} - палиндром");
     }
     else
     {
-        Console.WriteLine($"{number} - НЕ палиндром");
+        Console.WriteLine($"Нет. {number} - НЕ палиндром");
     }
 }
 else
 {
-    Console.WriteLine($"{number} - не является пятизначным");
+    Console.WriteLine($"Число {number} - не является пятизначным. Введите пятизначное число.");
 }

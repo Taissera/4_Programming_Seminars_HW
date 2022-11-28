c01dbadc31f6f04941baecfc692957c499f503e6
Taissera <taissera@ya.ru>
2022-11-28 23:12:41 +0300

    HW Seminar_3

---
+++ Seminar_3/Zadacha_21/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+// Вычисляется по формуле: AB = √(xb - xa)2 + (yb - ya)2 + (zb - za)2
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            isCorrect = true;
+        }
+    }
+    return result;
+}
+int x1 = GetNumber("ввод x1" );
+int x2 = GetNumber("ввод x2" );
+int y1 = GetNumber("ввод y1" );
+int y2 = GetNumber("ввод y2" );
+int z1 = GetNumber("ввод y1" );
+int z2 = GetNumber("ввод y2" );
+double sum1 = Math.Pow((x2 - x1), 2);
+double sum2 = Math.Pow((y2 - y1), 2);
+double sum3 = Math.Pow((z2 - z1), 2);
+double result = Math.Sqrt(sum1 + sum2 + sum3);
+Console.WriteLine(result);
---
+++ Seminar_3/Zadacha_23/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result) && result >= 1)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели НЕ число или 0. Введите целое число больше 0");
+        }
+    }
+    return result;
+}
+
+int lin = GetNumber("Введите число");
+for(int i = 1; i <= lin; i++)
+{
+    Console.Write($" {Math.Pow(i,3)}, ");
+}

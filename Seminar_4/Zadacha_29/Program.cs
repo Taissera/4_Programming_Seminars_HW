13b07fabe179ac3cc6342df9e15cb87cf355a946
taissera <taissera@ya.ru>
2022-12-08 01:41:03 +0300

    HW Seminar 4

--- Seminar_4/Zadacha_29/Program.cs
+++ Seminar_4/Zadacha_29/Program.cs
@@ -9,7 +9,7 @@ Random rnd = new Random();
 
 for (long i = 0; i < array.Length; i++)
 {
-    array[i] = new Random().Next();
+    array[i] = rnd.Next();
 // поскольку в условии задачи не указано сколько цифр должно быть в числе, я использую Next()
 // если нужно выводить двух или трёхзначные числа, то можно использовать Next(0,100) и Next(0,1000) соответственно
     //array[i] = new Random().Next(0,100);
